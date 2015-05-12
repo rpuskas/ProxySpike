284b05eefc7b8469957fb3de04d888300c46dd45
Ryan Puskas <ryanpuskas@gmail.com>
2015-05-12 03:45:47 -0700

    Adds missing client

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProxySpike")]
+[assembly: AssemblyTitle("Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ProxySpike")]
+[assembly: AssemblyProduct("Client")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3a8c6f4e-7448-4c6f-8818-1de04c86aeab")]
+[assembly: Guid("907d617a-2679-45a7-a334-fb1dd45b4ac0")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Client/TestClient.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.Http;
+using System.Reflection;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Client
+{
+    [TestFixture]
+    public class TestClient
+    {
+        [Test]
+        public void ShouldGetDataFromUnreferencedLibraryServer()
+        {
+            var barResult = new LibraryClient().Get();
+            Assert.AreEqual(barResult.Name,"FooResult");
+            Assert.AreEqual(barResult.Age,1);
+        }
+
+        [Test]
+        public void ShouldGetDataFromUnreferencedConsoleServer()
+        {
+            var barResult = new ConsoleClient().Get();
+            Assert.AreEqual(barResult.Name, "FooResult");
+            Assert.AreEqual(barResult.Age, 1);
+        }
+    }
+
+    public interface IProxy
+    {
+        BarResult Get();
+    }
+
+    public class LibraryClient : IProxy
+    {
+        private const string Path = @"c:\users\rpuskas\documents\visual studio 2013\Projects\ProxySpike\LibraryServer\bin\Debug\LibraryServer.dll";
+
+        public BarResult Get()
+        {
+            var result = Result("LibraryServer.Foo", "Get", null);
+            var serializeObject = JsonConvert.SerializeObject(result);
+            return JsonConvert.DeserializeObject<BarResult>(serializeObject);
+        }
+
+        private static object Result(string libraryserverFoo, string name, object[] objects)
+        {
+            var assembly = Assembly.LoadFile(Path);
+            var type = assembly.GetType(libraryserverFoo);
+            var instance = Activator.CreateInstance(type);
+            return type.InvokeMember(name, BindingFlags.InvokeMethod | BindingFlags.Instance | BindingFlags.Public, null, instance, objects);
+        }
+    }
+
+    public class ConsoleClient : IProxy
+    {
+        public BarResult Get()
+        {
+            var httpClient = new HttpClient();
+            var httpResponseMessage = httpClient.GetAsync("http://localhost:8081").Result;
+            var response = httpResponseMessage.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<BarResult>(response);
+        }
+    }
+
+    public class BarResult
+    {
+        public object Name { get; set; }
+        public int Age { get; set; }
+    }
+}
